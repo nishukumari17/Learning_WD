015c04934f46795e49ea9129860e4f00294d7ce4
nishukumari17 <nishuavni17@gmail.com>
2026-09-25 18:17:19 +0530

    Learning functions getterand setter and reduce method in js

--- Java_Script/functions.js
+++ Java_Script/functions.js
@@ -21,3 +21,33 @@ function intrest(p,r=5,t=5){
     return p*r*t/100;
 }
 console.log(intrest(1000));
+
+//Getter and setter
+
+let person={
+    fname:'Nishu',
+    lname:'Kumari',
+    //getter function
+    get fullName(){
+        return `${person.fname}  ${person.lname}`
+    },
+    set fullName(value){
+        let parts=value.split(' ');
+        this.fname=parts[0];
+        this.lname=parts[1];
+    }
+}
+
+//to call getter
+console.log(person.fullName);
+
+//to call setter
+person.fullName="Avni Singh";
+console.log(person.fullName);
+
+//Reduce Method
+
+let red=[1,2,3,4,5];
+let redSum=red.reduce((accumulator,currentValue)=>accumulator+currentValue);
+
+console.log(redSum);
